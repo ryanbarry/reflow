3e2b79882719baef690b9bcc5d801896312001f4
Ryan Barry <ryan@nuclearice.com>
2018-03-17 18:26:40 -0700

    centralize output control/pin definitions

---
+++ src/pins.cpp
@@ -0,0 +1,3 @@
+#include "pins.h"
+
+uint8_t output_pins[5] = {15, 23, 18, 16, 17};
--- src/pins.h
+++ src/pins.h
@@ -1,3 +1,8 @@
+#ifndef _PINS_H
+#define _PINS_H
+
+#include <stdint.h>
+
 //MAX38155 thermocouple interface
 #define M_CLK 13 //SCK0
 #define M_CS 6
@@ -12,3 +17,18 @@
 #define T_CS  8
 #define T_IRQ 4
 
+//outputs
+#define NUM_OUTPUTS 5
+#define NUM_HEATERS 3
+#define OUTPUT_PINS_HEATER_TOP 0
+#define OUTPUT_PINS_HEATER_BOTTOM 1
+#define OUTPUT_PINS_HEATER_BOOST 2
+#define OUTPUT_PINS_FAN_COOLING 3
+#define OUTPUT_PINS_SERVO_DOOR 4
+extern uint8_t output_pins[5];
+
+#define OUTPUT_ON(p) digitalWrite(output_pins[p], 1)
+#define OUTPUT_OFF(p) digitalWrite(output_pins[p], 0)
+#define ALL_HEATERS_OFF OUTPUT_OFF(OUTPUT_PINS_HEATER_TOP); OUTPUT_OFF(OUTPUT_PINS_HEATER_BOTTOM); OUTPUT_OFF(OUTPUT_PINS_HEATER_BOOST);
+
+#endif
