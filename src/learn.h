5d678aa6a18166e4f0541c77e4b191b86c4a47cb
Ryan Barry <ryan@nuclearice.com>
2018-04-01 12:21:15 -0700

    add values from learning cycle

--- src/learn.h
+++ src/learn.h
@@ -3,6 +3,14 @@
 
 #include "temp.h"
 
+#define LEARNED_POWER_WHOLE_OVEN 12
+#define LEARNED_POWER_BOTTOM 28
+#define LEARNED_POWER_TOP 23
+#define LEARNED_INERTIA_WHOLE_OVEN 43
+#define LEARNED_INERTIA_BOTTOM 111
+#define LEARNED_INERTIA_TOP 56
+#define LEARNED_INSULATION 154
+
 void learn(Thermocouple &tc);
 
 #endif
